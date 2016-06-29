5265f80c16694ba12f93ed4c36f9321733461303
PBalsetwar <PBalsetwar@VSPL1115CPU3.co.valudconsulting.com>
2016-06-29 15:48:31 +0530

    Assignment04 29/06/2016

---
+++ String4/src/main/Application.java
@@ -0,0 +1,16 @@
+package main;
+/**
+ * 
+ * @author Payal Balsetwar
+ *Date29/06/2016
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		ReadData rd=new ReadData();
+		rd.readString();
+
+
+	}
+
+}
---
+++ String4/src/main/ReadData.java
@@ -0,0 +1,53 @@
+package main;
+import java.util.Scanner;
+
+
+
+
+	import java.util.ArrayList;
+	import java.util.Arrays;
+
+	public class ReadData {
+		public void readString()
+		{
+	    Scanner sc = new Scanner( System.in );
+	    System.out.println( "Please enter a line of text" );
+	    String userInput = sc.nextLine();
+	    String head1="Word Length";
+	    String head2="Occurences";
+	    userInput = userInput.toLowerCase();
+
+	    //userInput = userInput.replaceAll( "\\W", " " );     
+	    //userInput = userInput.replaceAll( "  ", " " );      
+	                                                        
+	                                                        
+	    String[] tokens = userInput.split( " " );
+	    System.out.println( userInput );
+
+	    ArrayList< String > items = new ArrayList< String >();
+	    String arr[]=tokens;
+	    items.addAll( Arrays.asList( tokens ) );
+
+	    int count = 0;
+	    System.out.printf( " %-15s %15s %n " ,head1,head2);
+	    for( int i = 0; i < items.size(); i++ )
+	    {	    	
+	        
+	        for( int j = 0; j < items.size(); j++ )
+	        {
+	            if( items.get( i ).equals( items.get( j ) ) )
+	                count++;
+	            if( items.get( i ).equals( items.get( j ) ) && count > 1 )
+	                items.remove( j ); 
+	          
+	        }  
+	        
+	        System.out.printf(" %-15s %15s %n " ,arr[i].length(), count );
+	        count = 0;
+	        
+	    }
+	    System.out.println("\n");
+	    
+	}
+	}
+
